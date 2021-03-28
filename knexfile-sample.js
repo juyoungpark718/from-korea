550b21fa78ffdb656b78a4ca80c4e22213a1a4bc
juyoungpark718 <juyoung7018.park@gmail.com>
2021-03-29 08:50:48 +0900

    Setting db using knex
    - knex를 이용한 db 연결
    - knex의 Migration을 이용하여 db에 테이블 생성

---
+++ knexfile-sample.js
@@ -0,0 +1,12 @@
+module.exports = {
+  development:{
+    client:"mysql",
+    connection: {
+      host: '',
+      user     : '',
+      password : '',
+      database : '',
+      charset  : 'utf8',
+    },
+  }
+}
