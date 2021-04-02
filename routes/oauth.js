1ec96f46306305b64abfee6e38a31216d40fbe8e
juyoungpark718 <juyoung7018.park@gmail.com>
2021-04-02 19:32:49 +0900

    google oauth 구현
    - service, router 분리

---
+++ routes/oauth.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const oauthService = require("../services/oauth");
+const _ = require("fxjs/Strict");
+
+router.get('/google/redirect', async (req, res, next) => {
+  await _.go(
+    req,
+    oauthService.getUserProfileGoogle,
+    oauthService.loginUser,
+    (user) => req.session.user = user
+  );
+  res.redirect("/")
+});
+
+module.exports = router;
