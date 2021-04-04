6988ee5d65f67709f2f9b2bf79b1b36927aebca5
juyoungpark718 <juyoung7018.park@gmail.com>
2021-04-05 08:58:07 +0900

    [feat] user db에 fbId 컬럼 추가
    - 페이스북 로그인은 fbId를 검사하고, google 로그인은 email을 찾음

--- migrations/20210330090406_create_table_user.js
+++ migrations/20210330090406_create_table_user.js
@@ -2,10 +2,11 @@
 exports.up = function(knex) {
   return knex.schema.createTable('users', (table) => {
     table.increments();
-    table.string('email').notNullable();
+    table.string('email').nullable();
     table.string('password').nullable();
-    table.string('picture').nullable();
+    table.string('picture', 350).nullable();
     table.string('name').notNullable();
+    table.string('fbId').nullable();
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at');
   });
