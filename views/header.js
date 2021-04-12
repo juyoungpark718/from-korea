abf927c967eb7486fa7b7fd79cbaf24a6ab67fdb
juyoungpark718 <juyoung7018.park@gmail.com>
2021-04-12 16:00:48 +0900

    [refactor] layout에서 header를 분리
    - header에서 로그인 / 로그아웃을 표시해줘야하기 때문에 분리

---
+++ views/header.js
@@ -0,0 +1,71 @@
+const publicHeader = () => {
+  return `
+  <header class="header">
+    <div class="header__top">
+      <div class="header__top__menu">
+        <a href="/user"><div class="header__top__menu__item">login</div></a>
+        <div class="header__top__menu__item">Anouncement</div>
+        <div class="header__top__menu__item">Help Center</div>
+      </div>
+    </div>
+    <div class="header__inner">
+      <div class="header__log">From Korea</div>
+      <div class="header__search">
+        <input class="header__search__input" type="text" placeholder="Search / Paste Url" />
+      </div>
+      <div class="header__user">
+        <img src="./images/reorder-four-outline.svg" />
+        <div class="menu">
+          <div class="menu__inner">
+            <div class="menu__inner__item--mobile">로고</div>
+            <div class="menu__inner__item--mobile">로그인</div>
+            <div class="menu__inner__item">My Cart</div>
+            <div class="menu__inner__item">My page</div>
+            <div class="menu__inner__item">My Wallet</div>
+            <div class="menu__inner__item--mobile">Help Center</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>`;
+}
+
+const privateHeader = (user) => {
+  return `
+  <header class="header">
+    <div class="header__top">
+      <div class="header__top__menu">
+        <a href="/user/logout"><div class="header__top__menu__item">logout</div></a>
+        <div class="header__top__menu__item">Anouncement</div>
+        <div class="header__top__menu__item">Help Center</div>
+      </div>
+    </div>
+    <div class="header__inner">
+      <div class="header__log">From Korea</div>
+      <div class="header__search">
+        <input class="header__search__input" type="text" placeholder="Search / Paste Url" />
+      </div>
+      <div class="header__user">
+        <img src="./images/reorder-four-outline.svg" />
+        <div class="menu">
+          <div class="menu__inner">
+            <div class="menu__inner__item--mobile">로고</div>
+            <a href="/user/logout"><div class="menu__inner__item--mobile">로그아웃</div></a>
+            <div class="menu__inner__item">My Cart</div>
+            <div class="menu__inner__item">My page</div>
+            <div class="menu__inner__item">My Wallet</div>
+            <div class="menu__inner__item--mobile">Help Center</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>`;
+}
+
+const render = (user) => {
+  return user ? privateHeader(user) : publicHeader();
+}
+
+module.exports = {
+  render
+}
