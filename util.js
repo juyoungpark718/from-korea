e4a07bf228abf390d5d60fdd8d89d1435333e3be
juyoungpark718 <juyoung7018.park@gmail.com>
2021-04-12 15:57:48 +0900

    [feat] styleSheet, scripts 함수 구현
    - css, script 태그를 path에 맞게 생성

---
+++ util.js
@@ -0,0 +1,5 @@
+global.styleSheet = (path) => `<link rel="stylesheet" type="text/css" href="/assets/${path}">`
+
+global.scripts = (path) => `<script type="module" src="/assets/${path}"></script>`
+
+
