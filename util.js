8836849eba69d5b7f1b9268fb9d1a47e3b58ec86
juyoungpark718 <juyoung7018.park@gmail.com>
2021-06-23 11:09:28 +0900

    [feat] cartoptions, hashcode util function 구현
    - hashcode는 product에 대한 구별값으로 사용

--- util.js
+++ util.js
@@ -1,4 +1,27 @@
+const _ = require("fxjs/Strict");
+const L = require("fxjs/Lazy");
+const fetch = require("node-fetch");
+
 global.styleSheet = (path) => `<link rel="stylesheet" type="text/css" href="/assets/${path}">`;
 
-global.scripts = (path) => `<script type="module" src="/assets/${path}"></script>`;
+global.script = (path) => `<script type="module" src="/assets/${path}"></script>`;
 
+global.hashCode = (str) => _.go(
+  str,
+  _.split(''),
+  strArr => _.reduce((a,b) => {
+    let code = b.charCodeAt(0);
+    a = (a << 6&268435455) + code + (code << 14);
+    let c = a & 266338304;
+    return c !==0 ? a^c >> 21 : a;
+  }, 0, strArr),
+  String
+);
+
+global.getCartOptions = (cart) => _.go(
+  L.range(Infinity),
+  L.map(i => `option${i + 1}`),
+  L.map(option => cart[option]),
+  _.takeWhile(v => v !== undefined),
+  _.join("%")
+);
